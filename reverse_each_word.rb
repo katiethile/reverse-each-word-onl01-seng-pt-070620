36f4894b3dc5fc1d5d8961190495b66b4e7984f0
Katie Le <lethikatie@gmail.com>
2020-07-14 00:34:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(words)
   correct_sentence = []
   words.split.collect do |word|
-  correct_sentence << (reversed_sentence.reverse)
+  correct_sentence << (word.reverse)
   end
   correct_sentence.join(" ")
 end
